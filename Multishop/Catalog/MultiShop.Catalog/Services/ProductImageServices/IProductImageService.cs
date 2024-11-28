0653f67e62c5669907d3264e6a882073c8518aa4
Enes Başpınar <Menesbaspinar@hotmail.com>
2024-11-28 23:15:24 +0300

    feat: IProductImageService interface eklendi ve Task tabanlı metodlar yazıldı
    Ürün görsel detayları ile ilgili CRUD işlemleri için async metodlar içeren yeni bir IProductImageService interface eklendi.

---
+++ Multishop/Catalog/MultiShop.Catalog/Services/ProductImageServices/IProductImageService.cs
@@ -0,0 +1,21 @@
+﻿using MultiShop.Catalog.Dtos.ProductImageDtos;
+
+namespace MultiShop.Catalog.Services.ProductImageServices
+{
+    public interface IProductImageService
+    {
+
+        Task<List<ResultProductImageDto>> GetProductImagesAsync();
+         
+        Task CreateProductImage(CreateProductImageDto productImageDto);
+
+        Task UpdateProductImageAsync(UpdateProductImageDto productImageDto);
+
+        Task DeleteProductImageAsync(string id);
+
+        Task<GetByIdProductImageDto> GetByIdProductImageAsync(string id);
+
+
+
+    }
+}
