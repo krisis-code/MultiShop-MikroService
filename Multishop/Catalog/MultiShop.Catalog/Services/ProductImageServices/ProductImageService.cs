e736f752ea1bd7788abc990f192c82f30581f377
Enes Başpınar <Menesbaspinar@hotmail.com>
2024-11-29 23:43:56 +0300

    ProductImageService sınıfı eklendi ve CRUD metodları yazıldı
    IProductImageService arayüzünü implement eden, MongoDB ile CRUD işlemleri gerçekleştiren ProductImageService  sınıfı oluşturuldu.
    Async işlemler ve AutoMapper kullanıldı.

---
+++ Multishop/Catalog/MultiShop.Catalog/Services/ProductImageServices/ProductImageService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.ProductImageDtos;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
+
+namespace MultiShop.Catalog.Services.ProductImageServices
+{
+    public class ProductImageService : IProductImageService
+    {
+        private readonly IMongoCollection<ProductImage> _productColletion;
+        private readonly IMapper _mapper;
+
+        public ProductImageService(IMapper mapper , IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var databse = client.GetDatabase(_databaseSettings.DatabaseName);
+            _productColletion = databse.GetCollection<ProductImage>(_databaseSettings.ProductDetailCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task CreateProductImage(CreateProductImageDto productImageDto)
+        {
+            var value = _mapper.Map<ProductImage>(productImageDto);
+            await _productColletion.InsertOneAsync(value);
+        }
+
+        public async Task DeleteProductImageAsync(string id)
+        {
+            await _productColletion.DeleteOneAsync(x => x.ProductId == id);
+        }
+
+        public async Task<GetByIdProductImageDto> GetByIdProductImageAsync(string id)
+        {   
+            var value = await _productColletion.Find(x => x.ProductId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdProductImageDto>(value);
+        }
+
+        public async Task<List<ResultProductImageDto>> GetProductImagesAsync()
+        {
+           var values = await _productColletion.Find(x=> true).ToListAsync();
+            return _mapper.Map<List<ResultProductImageDto>>(values);
+        }
+
+        public async Task UpdateProductImageAsync(UpdateProductImageDto productImageDto)
+        {
+            var values = _mapper.Map<ProductImage>(productImageDto);
+            await _productColletion.FindOneAndReplaceAsync(x => x.ProductId == productImageDto.ProductImageId, values);
+        }
+    }
+}
