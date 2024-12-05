81b13c470cb2eb6ef4899e334212a173cda2dca5
Enes Başpınar <Menesbaspinar@hotmail.com>
2024-12-05 17:29:03 +0300

    Discount servisi ve interfaci eklendi ve implamente edildi

---
+++ Multishop/Discount/Multishop.Discount/Services/DiscountService.cs
@@ -0,0 +1,32 @@
+﻿using Multishop.Discount.Dtos;
+
+namespace Multishop.Discount.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        public Task CreateCoupon(ResultCouponDto coupon)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCoupon(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCouponDto>> GetAllCoupons()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCouponDto> GetByIdCoupon(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCoupon(ResultCouponDto coupon)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Multishop/Discount/Multishop.Discount/Services/IDiscountService.cs
@@ -0,0 +1,17 @@
+﻿using Multishop.Discount.Dtos;
+
+namespace Multishop.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<List<ResultCouponDto>> GetAllCoupons();
+        
+        Task CreateCoupon(ResultCouponDto coupon);
+
+        Task UpdateCoupon(ResultCouponDto coupon);
+
+        Task DeleteCoupon(int id);
+        
+        Task<GetByIdCouponDto> GetByIdCoupon(int id);
+    }
+}
